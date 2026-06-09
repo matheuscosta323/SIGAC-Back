51e6c69b318f9de43000c12b808714d14a7f76c7
matheuscosta323 <matheuscosta323@gmail.com>
2026-06-08 23:31:49 -0300

    Update aluno_curso.py

--- app/models/aluno_curso.py
+++ app/models/aluno_curso.py
@@ -1,15 +1,14 @@
 from app.extensions import Base
-from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class AlunoCurso(Base):
     __tablename__ = "aluno_curso"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     id_aluno: Mapped[int] = mapped_column(ForeignKey("usuario.id"), nullable=False)
     id_curso: Mapped[int] = mapped_column(ForeignKey("curso.id"), nullable=False)
 
     __table_args__ = (
-        UniqueConstraint("id_aluno", "id_curso", name="uq_aluno_curso"),
+        PrimaryKeyConstraint("id_aluno", "id_curso"),
     )
