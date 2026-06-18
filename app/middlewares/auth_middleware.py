7cdacb1bec10b178cc88aaf50d4f2c81c51fef56
matheuscosta323 <matheuscosta323@gmail.com>
2026-06-18 08:49:15 -0300

    Update auth_middleware.py

--- app/middlewares/auth_middleware.py
+++ app/middlewares/auth_middleware.py
@@ -1,4 +1,3 @@
-from functools import wraps
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
 
 
@@ -6,7 +5,6 @@ def verificar_role(roles_permitidas):
 
     def decorator(function):
 
-        @wraps(function)  # preserva __name__, __doc__, __module__ — evita conflito de endpoints no Flask
         def wrapper(*args, **kwargs):
             verify_jwt_in_request()
             role = get_jwt().get("role")
@@ -16,7 +14,7 @@ def verificar_role(roles_permitidas):
                     "message": "Acesso negado."
                 }, 403
             return function(*args, **kwargs)
-
+            wrapper.__name__ == function.__name__
         return wrapper
 
     return decorator
